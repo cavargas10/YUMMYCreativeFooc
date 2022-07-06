514e98255585a027a7402dc4483e158c12001e47
Carlos Vargas R <carvarjr_95@hotmail.com>
2022-07-06 01:32:03 -0500

    Dropdown php

--- ProyectoWeb/vistas_usuario/tips_info_user.php
+++ ProyectoWeb/vistas_usuario/tips_info_user.php
@@ -10,6 +10,7 @@ include("../seguridad/seguridad.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Yummi Creative Food</title>
     <link rel="stylesheet" href="../css/tips_info.css " />
+    <link rel="stylesheet" href="../css/dropdown_User.css" />
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
     <script src="js/main.js"></script>
 </head>
